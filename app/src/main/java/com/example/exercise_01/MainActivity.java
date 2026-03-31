60f060724c6fb82e6dbc0183fc202b27af3095a3
Eron <aarondamaolao13@gmail.com>
2026-03-31 12:43:38 +0800

    Completed Part 2: Activity Lifecycle and Counter

--- app/src/main/java/com/example/exercise_01/MainActivity.java
+++ app/src/main/java/com/example/exercise_01/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,32 +14,68 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mCounter = 0;
+    private TextView tvCounter;
+    private static final String COUNT_KEY = "COUNT_KEY";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        
+        View mainView = findViewById(R.id.main);
+        ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            // Only update padding if it has actually changed to avoid layout loops
+            if (v.getPaddingLeft() != systemBars.left || v.getPaddingTop() != systemBars.top ||
+                v.getPaddingRight() != systemBars.right || v.getPaddingBottom() != systemBars.bottom) {
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            }
             return insets;
         });
 
-        // Hardcode Student ID into the Action Bar Title
+        tvCounter = findViewById(R.id.tvCounter);
+        tvCounter.setText(String.valueOf(mCounter));
+
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("ID: 23103014");
         }
 
-        // Initialize UI components
+
         Button btnChangeName = findViewById(R.id.btnChangeName);
         final TextView tvDepartment = findViewById(R.id.tvDepartment);
+        Button btnIncrement = findViewById(R.id.btnIncrement);
 
-        // Set Click Listener to change the TextView to the Chairwoman's name
         btnChangeName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tvDepartment.setText("Angie Ceniza");
             }
         });
+
+        btnIncrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCounter++;
+                tvCounter.setText(String.valueOf(mCounter));
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the current counter value
+        outState.putInt(COUNT_KEY, mCounter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore the counter value after rotation
+        mCounter = savedInstanceState.getInt(COUNT_KEY);
+        tvCounter.setText(String.valueOf(mCounter));
     }
 }
